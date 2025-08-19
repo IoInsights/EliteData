f28488deb55e2da42e8329d38de6a32d702e85cd
Tarcis Tobias <tarcistt@gmail.com>
2025-08-19 12:07:41 -0300

    atualizaçãoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -195,3 +195,17 @@ document.getElementById("imageModal").onclick = function(e) {
         this.style.display = "none";
     }
 };
+
+
+//FAQ
+
+
+document.querySelectorAll(".faq-list details").forEach((item) => {
+    item.addEventListener("toggle", () => {
+        if (item.open) {
+            document.querySelectorAll(".faq-list details").forEach((other) => {
+                if (other !== item) other.removeAttribute("open");
+            });
+        }
+    });
+});
